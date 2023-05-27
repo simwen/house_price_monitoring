ba6e6afd8e07de9bcadb7e5fb2a800087bda4202
Sim Wentzel <swentzel4@gmail.com>
2023-05-27 14:11:25 +0100

    combining 0.5 and 1mile scrape

--- Monitor_funcs.py
+++ Monitor_funcs.py
@@ -9,30 +9,33 @@ Created on Sun Nov  6 13:26:07 2022
 import requests
 from bs4 import BeautifulSoup
 import time
-import numpy as np
 from datetime import datetime
 
-# Scraping the rightmove property search results webpages 
-# Collates valid properties' weblinks, plus their price and id
+# Scraping the rightmove search results webpages 
+# Collates valid properties' weblinks, price and id
 
-def scrape_results_page(min_beds=2,max_beds=2,radius=1,noPages=2,days_since_added=7):
+# Input here the area/station codes you're interested in, and the radius of the search in the form {area: radius}
+# Area and station codes can be found in the weblink of a rightmove property search
+areas = {'STATION%5E7832': 1,'STATION%5E3509': 1, 'STATION%5E9338': 1, 'STATION%5E245': 1, 'STATION%5E6380': 0.5}
+
+def scrape_results_page(min_beds=2,max_beds=2,noPages=2,days_since_added=7):
     all_apartment_links = [] # stores apartment links
     all_price = [] # stores the listing price of apartment
-    all_featured = []
-    all_datetime = []
-    stations = ['STATION%5E7832','STATION%5E3509', 'STATION%5E9338', 'STATION%5E245']
-    
-    ## Main scraper for 1 mile radius stations
-    
-    for station in stations:
+    all_featured = [] # stores 'featured' status of properties
+    all_datetime = [] # stores date of scrape
+        
+    for area_key in areas:
+        print(f'scraping {area_key}')
         for i in range(noPages):
             if i==0:
-                r= requests.get(f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={station}&maxBedrooms={max_beds}&minBedrooms={min_beds}&radius={radius}&sortType=6&propertyTypes=detached%2Cflat%2Csemi-detached%2Cterraced&maxDaysSinceAdded={days_since_added}&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=')
+                r= requests.get(f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={area_key}&maxBedrooms={max_beds}&minBedrooms={min_beds}&radius={areas[area_key]}&sortType=6&propertyTypes=detached%2Cflat%2Csemi-detached%2Cterraced&maxDaysSinceAdded={days_since_added}&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=')
+            elif i>0 and areas[area_key] <1:  
+                break    # usually 0.5 radius searches have only 1 page of properties
             else:
                 r = ''
                 while r == '':
                     try:
-                        r = requests.get(f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={station}&maxBedrooms={max_beds}&minBedrooms={min_beds}&radius={radius}&sortType=6&index={i*24}&propertyTypes=detached%2Cflat%2Csemi-detached%2Cterraced&maxDaysSinceAdded={days_since_added}&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=')
+                        r = requests.get(f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={area_key}&maxBedrooms={max_beds}&minBedrooms={min_beds}&radius={areas[area_key]}&sortType=6&index={i*24}&propertyTypes=detached%2Cflat%2Csemi-detached%2Cterraced&maxDaysSinceAdded={days_since_added}&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=')
                         break
                     except:
                         print(f'Connection refused by the server on page {i+1}... sleeping for 3 seconds')
@@ -75,51 +78,8 @@ def scrape_results_page(min_beds=2,max_beds=2,radius=1,noPages=2,days_since_adde
                 # append date
                 dt_string = datetime.now().strftime("%d/%m/%Y %H:%M")
                 all_datetime.append(dt_string)
- 
-    
-    ## Secondary scraper for 0.5 mile radius station
-
-    r= requests.get(f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=STATION%5E6380&maxBedrooms={max_beds}&minBedrooms={min_beds}&radius=0.5&sortType=6&propertyTypes=detached%2Cflat%2Csemi-detached%2Cterraced&maxDaysSinceAdded={days_since_added}&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=')
-    soup = BeautifulSoup(r.text, 'lxml')
-
-    apartments = soup.find_all("div", class_="l-searchResult is-list")
-
-    for j in range(len(apartments)):
-
-        # tracks which apartment we are on in the page
-        apartment_no = apartments[j]
-
-        # append link
-        apartment_info = apartment_no.find("a", class_="propertyCard-link")
-        link = "https://www.rightmove.co.uk" + apartment_info.attrs["href"]
-        all_apartment_links.append(link)
-
-        # append price
-        price = (
-            apartment_no.find("div", class_="propertyCard-priceValue")
-            .get_text()
-            .strip()
-        )
-        all_price.append(int(price.replace(",","").replace("£","")))
-        
-        # append featured listing indicator
-        featured = (
-            apartment_no.find("div", class_="propertyCard-moreInfoFeaturedTitle")
-            .get_text()
-        )
-        if len(featured) > 0:
-            featured = 1
-        else:
-            featured = 0
-        all_featured.append(featured)
-        
-        # append date
-        dt_string = datetime.now().strftime("%d/%m/%Y %H:%M")
-        all_datetime.append(dt_string)
-    
-    #remove_indices = [0,25]
-    #all_apartment_links = [i for j, i in enumerate(all_apartment_links) if j not in remove_indices]
-    #all_price = [i for j, i in enumerate(all_price) if j not in remove_indices]
-    #all_id_no = [i for j, i in enumerate(all_id_no) if j not in remove_indices]
     
     return r.ok, all_apartment_links, all_datetime, all_price, all_featured
+
+
+
